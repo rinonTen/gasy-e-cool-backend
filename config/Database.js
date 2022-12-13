7a89cca147f3975b8e76ebd86bd30f8f19111f91
Rinon <rinon.ten@filament.uk.com>
2022-12-13 16:39:14 +0300

    Put password in .env file

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('e_gasy_cool', 'root', 'Rino@Mahanoro123', {
+const db = new Sequelize('e_gasy_cool', 'root', process.env.DB_PASSWORD, {
     host: "localhost",
     dialect: "mysql"
 });
