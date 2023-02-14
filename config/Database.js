9040b069814c3f001209e466be0bdabc07c018d9
Rinon <rinon.ten@filament.uk.com>
2023-02-14 09:40:08 +0300

    Update host

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
  
 const db = new Sequelize('e_gasy_cool', 'root', process.env.DB_PASSWORD || 'Rino@Mahanoro123', {
-    host: ["localhost", "https://e-gasy-shops.onrender.com", 'e-gasy-shops.onrender'],
+    host: "127.0.0.1",
     dialect: "mysql"
 });
  
