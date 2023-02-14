e4067d3400995dd270cb52ca110e237733bd20f7
Rinon <rinon.ten@filament.uk.com>
2023-02-14 09:10:26 +0300

    Removed unused pieces of code

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import bodyParser from "body-parser";
 import router from "./routes/index.js";
-dotenv.config();
+const PORT = process.env.PORT || 4000;
+
 const app = express();
- 
-app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
+dotenv.config();
+// Extend request's size limit
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+
+app.use(cors({ credentials:true, origin: ['http://localhost:3000', 'https://gasy-e-cool.vercel.app'] }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
  
-app.listen(4000, ()=> console.log('Server running at port 4000'));
+app.listen(PORT, ()=> console.log('Server running at port 4000'));
